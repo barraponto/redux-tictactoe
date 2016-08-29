3a7db5cea2ca69290d3bea87c80bff26f5663e65
Capi Etheriel <barraponto@gmail.com>
2016-08-29 17:11:10 -0300

    [actions] update tests to support player index in play action

--- test/actions.js
+++ test/actions.js
@@ -8,12 +8,12 @@ var actions = require('../src/actions');
 describe('Actions', function(){
   it('should play on the first cell', function(){
     // var action = {type: actions.PLAY, player: 'hackerman', cell: 0};
-    var action = actions.play('hackerman', 0);
+    var action = actions.play(0, 0);
     action.should.be.an('Object');
     action.type.should.be.a('String');
     action.type.should.equal(actions.PLAY);
-    action.player.should.be.a('String');
-    action.player.should.equal('hackerman');
+    action.player.should.be.a('Number');
+    action.player.should.equal(0);
     action.cell.should.be.a('Number');
     action.cell.should.equal(0);
   });
