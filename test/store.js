07a09c12314e570352bb99ad8ba81e64218e0bc3
Capi Etheriel <barraponto@gmail.com>
2016-08-29 18:03:19 -0300

    [store] further test store support for play actions

--- test/store.js
+++ test/store.js
@@ -1,5 +1,6 @@
 var should = require('chai').should();
 var Store = require('../src/store');
+var actions = require('../src/actions');
 
 describe('Store', function(){
   it('should initialize store with default state', function(){
@@ -12,5 +13,29 @@ describe('Store', function(){
     var store = Store.create();
     var state = store.getState();
     state.should.deep.equal(expected);
+  });
+
+  it('should update state on play actions', function(){
+    var expected = {
+        turn: 1,
+        board: [ 0, null, null, null, null, null, null, null, null ],
+        players: [ { id: null }, { id: null } ],
+        winner: null
+    };
+
+    var store = Store.create();
+    store.dispatch(actions.play(0, 0));
+    var state = store.getState();
+    state.should.deep.equal(expected);
+
+    var nextExpected = {
+        turn: 2,
+        board: [ 0, 1, null, null, null, null, null, null, null ],
+        players: [ { id: null }, { id: null } ],
+        winner: null
+    };
+    store.dispatch(actions.play(1, 1));
+    var nextState = store.getState();
+    nextState.should.deep.equal(nextExpected);
   });
 });
